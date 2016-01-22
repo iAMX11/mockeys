a395027af2442d1bf572d939fe27c8a97dbffe75
mafflin <mafflin@gmail.com>
2016-01-22 23:29:56 +0300

    Version 6. 3

--- extension.js
+++ extension.js
@@ -4,10 +4,6 @@ const Util = imports.misc.util;
 
 let button;
 
-function _mocPlay() {
-  Util.spawn(['mocp', '-p']);
-}
-
 function _mocPause() {
   Util.spawn(['mocp', '-G']);
 }
@@ -27,27 +23,21 @@ function init() {
     button1.set_child(icon);
     button1.connect('button-press-event', _mocPrev);
 
-    button2 = new St.Button({ style_class: 'panel-button'});
-    let icon = new St.Icon({ icon_name: 'media-playback-start-symbolic',
-                             style_class: 'system-status-icon' });
-    button2.set_child(icon);
-    button2.connect('button-press-event', _mocPlay);
 
-    button3 = new St.Button({ style_class: 'panel-button'});
+    button2 = new St.Button({ style_class: 'panel-button'});
     let icon = new St.Icon({ icon_name: 'media-playback-pause-symbolic',
                              style_class: 'system-status-icon' });
-    button3.set_child(icon);
-    button3.connect('button-press-event', _mocPause);
+    button2.set_child(icon);
+    button2.connect('button-press-event', _mocPause);
 
-    button4 = new St.Button({ style_class: 'panel-button'});
+    button3 = new St.Button({ style_class: 'panel-button'});
     let icon = new St.Icon({ icon_name: 'media-skip-forward-symbolic',
                              style_class: 'system-status-icon' });
-    button4.set_child(icon);
-    button4.connect('button-press-event', _mocNext);
+    button3.set_child(icon);
+    button3.connect('button-press-event', _mocNext);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button4, 2);
     Main.panel._rightBox.insert_child_at_index(button3, 2);
     Main.panel._rightBox.insert_child_at_index(button2, 2);
     Main.panel._rightBox.insert_child_at_index(button1, 2);
@@ -57,5 +47,4 @@ function disable() {
     Main.panel._rightBox.remove_child(button1);
     Main.panel._rightBox.remove_child(button2);
     Main.panel._rightBox.remove_child(button3);
-    Main.panel._rightBox.remove_child(button4);
 }
